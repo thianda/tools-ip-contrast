e41d2317b0a82df0ea546b10791af3145b63bb44
thianda <thianda91@outlook.com>
2018-12-04 01:28:59 +0800

    v0.4 done: generate temp files

--- ip-contrast/ip-contrast.py
+++ ip-contrast/ip-contrast.py
@@ -9,6 +9,7 @@ import openpyxl
 
 config = configparser.ConfigParser()
 configFileName = "config.ini"
+t0 = 0
 
 
 # ip 转换： int 转 str
@@ -28,9 +29,9 @@ def ipParse(ipStr):
     maskLen = 32
     if "/" in ipStr:
         ipStr, maskLen = ipStr.split("/")
-    print(ipStr, maskLen)
+    # print(ipStr, maskLen)
     mask = 0xFFFFFFFF << (32 - int(maskLen))
-    print("mask:", mask)
+    # print("mask:", mask)
     ipInt = ip2int(ipStr)
     ipStart = ipInt & mask & 0xFFFFFFFF
     ipEnd = ipInt | ~mask & 0xFFFFFFFF
@@ -98,7 +99,7 @@ def writeConfig(configFileName):
     config.set("工信部备案", "field7", "使用单位名称")
     with open(configFileName, "w") as configFile:
         configFile.write(
-            "# Author: Xianda\n\n# 本配置为一致性检查工具的配置\n# 如删除本配置文件，重新生成的配置文件即为默认配置\n# 如需修改配置，修改本文件后直接保存即可\n\n######\n\n# 若 IP 地址字段分为起始IP、终止IP的，`ip` 字段中用“;”(英文分号)隔开\n# 程序会依次对 field 字段进行对比并输出对比结果\n# field 字段有变化可直接在此增删改，满足一一对应即可\n\n\n\n"
+            "# Author: Xianda\n\n# 本配置为一致性检查工具的配置\n# 如删除本配置文件，重新生成的配置文件即为默认配置\n# 如需修改配置，修改本文件后直接保存即可\n\n######\n\n# before 表示数据的起始行（列名占用的行数）\n# 若 IP 地址字段分为起始IP、终止IP的，`ip` 字段中用“;”(英文分号)隔开\n# 程序会依次对 field 字段进行对比并输出对比结果\n# field 字段有变化可直接在此增删改，满足一一对应即可\n\n\n\n"
         )
         config.write(configFile)
 
@@ -154,12 +155,13 @@ def generateTemp(fileName):
     options = {}
     colNames = {}
     ipNames = {}
+    sheet = {}
     for k, v in fileName.items():
         xls = xlrd.open_workbook(v)
         # 获取最后一个 sheet
-        sheet = xls.sheet_by_index(len(xls.sheet_names()) - 1)
+        sheet[k] = xls.sheet_by_index(len(xls.sheet_names()) - 1)
         # 默认将第一行做为列名所在的行
-        Row0 = sheet.row_values(0)
+        Row0 = sheet[k].row_values(0)
         options[k] = {}
         options[k]['fieldCols'] = {}
         options[k]['ipCols'] = []
@@ -188,7 +190,8 @@ def generateTemp(fileName):
                             options[k]['fieldCols'][field] = i
                             colNames[k].append(cellValue)
                         # 此处不能加 break，否则匹配到`起始IP`即跳出 for 循环，无法匹配`结束IP`
-        colNames[k] = ['ipStart', 'ipEnd'] + colNames[k]
+        # colNames[k] = ['ipStart', 'ipEnd'] + colNames[k]
+        colNames[k].extend(['ipStart', 'ipEnd'])
         colNames[k].extend(ipNames[k])
 
     # os.mkdir('\\_temp')
@@ -216,24 +219,52 @@ def generateTemp(fileName):
     cell.font = Font(size=11, color='8060ee', bold=True)
     ws0.append([None, None, None, None, None, None, cell])
     # ws.merge_cells('A2:G3')
+    wrName = configPath + currDate + '/temp' + str(time.time()) + '.xlsx'
     for k, v in fileName.items():
+        print('Temp File is being Generated for:', k, v)  # debug
         # 为每个对比文件创建中间文件的一个sheet
         ws = wb.create_sheet(k)
         # todo: 输出中间文件 or 直接对比并给出结果
         ws.append(colNames[k])
-    wrName = configPath + currDate + '/temp' + str(time.time()) + '.xlsx'
+        # 遍历每一行
+        for row in range(int(options[k]['before']), sheet[k].nrows):
+            tempRow = []
+            rowValues = sheet[k].row_values(row)
+            fieldCols = options[k]['fieldCols']
+            for i in fieldCols:
+                tempRow.append(rowValues[int(fieldCols[i])])
+            ipCols = options[k]['ipCols']
+            if len(ipCols) == 1:
+                ipStr = rowValues[ipCols[0]]
+                tempRow.extend([ipParse(ipStr)[2], ipParse(ipStr)[3], ipStr])
+            elif len(ipCols) == 2:
+                # 导出数据自带起始 IP 结束IP
+                ip1Str = rowValues[ipCols[0]]
+                ip2Str = rowValues[ipCols[1]]
+                ip1 = ip2int(ip1Str)
+                ip2 = ip2int(ip2Str)
+                if ip1 < ip2:
+                    tempRow.extend([ip1, ip2, ip1Str, ip2Str])
+                else:
+                    tempRow.extend([ip2, ip1, ip2Str, ip1Str])
+            else:
+                print('IP 列识别错误')
+            # print('tempRow', tempRow) # debug
+            ws.append(tempRow)
+            # 遍历 fileNames 的第一个文件，逐行计算 ipStart、ipEnd，并查找一致信息
     wb.save(wrName)
-    print("options", options)
-    print('colNames', colNames)
+    print("options", options, '\n')
+    print('colNames', colNames, '\n')
     return wrName
 
 
 def contrast():
     # 获取导出文件的文件名
     fileName = matchedFileName()
+    print("fileName", fileName, '\n')
     tempFile = generateTemp(fileName)
-    print("fileName", fileName)
     print('tempFile', tempFile)
+
     return
     # 根据配置，第一个字段为查询索引，对比其余字段是否一致
 
@@ -273,5 +304,5 @@ if __name__ == "__main__":
     t0 = time.time()
     initConfig()
     t = time.time()-t0
-    print('脚步执行用时：%2.3f s' % t)
+    print('本工具执行用时：%2.3f s' % t)
     os.system('pause')
